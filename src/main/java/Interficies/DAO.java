59277f9ab72846205d7ef367fbcf293255967cea
RealPerpy18 <arpeto@inspalamos.ct>
2022-11-29 16:31:22 +0100

    Dao arreglao

--- src/main/java/Interficies/DAO.java
+++ src/main/java/Interficies/DAO.java
@@ -2,74 +2,75 @@ package Interficies;
 
 import hybernates.ORM_DEF.*;
 
+import javax.persistence.EntityManager;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface DAO {
-    ArrayList<Client> TotsCli(Connection con);
-    Client cercaClient(String dni, Connection con);
-    Client cercaClient(int id, Connection con);
+    ArrayList<Client> TotsCli(EntityManager entity);
+    Client cercaClient(String dni, EntityManager entity);
+    Client cercaClient(int id, EntityManager entity);
 
-    boolean createClient(Client cli, Connection con);
-    boolean updateClient(Client cli, Connection con);
-    boolean deleteClient(Client cli, Connection con);
+    boolean createClient(Client cli, EntityManager entity);
+    boolean updateClient(Client cli, EntityManager entity);
+    boolean deleteClient(Client cli,EntityManager entity);
 
-    ArrayList<Bitllet> TotsBit(Connection con);
-    Bitllet cercaBitllet(int id, Connection con);
+    ArrayList<Bitllet> TotsBit(EntityManager entity);
+    Bitllet cercaBitllet(int id, EntityManager entity);
 
-    boolean createBitllet(Bitllet bit, Connection con);
-    boolean updateBitllet(Bitllet bit, Connection con);
-    boolean deleteBitllet(Bitllet bit, Connection con);
-
-
-
-    ArrayList<Compra> TotsCom(Connection con);
-    Compra cercaCompra(int id, Connection con);
-
-    boolean createCompra(Compra com, Connection con);
-    boolean updateCompra(Compra com, Connection con);
-    boolean deleteCompra(Compra com, Connection con);
-
-
-    ArrayList<Equipatge> TotsEquip(Connection con);
-    Equipatge cercaEquipatge(int id, Connection con);
-
-    boolean createEquipatge(Equipatge equ, Connection con);
-    boolean updateEquipatge(Equipatge equ, Connection con);
-    boolean deleteEquipatge(Equipatge equ, Connection con);
-
-    ArrayList<FacEquip> TotsFequip(Connection con);
-    FacEquip cercaFacEquipatge(int id, Connection con);
-
-    boolean createFacEquipatge(FacEquip feq, Connection con);
-    boolean updateFacEquipatge(FacEquip feq, Connection con);
-    boolean deleteFacEquipatge(FacEquip feq, Connection con);
+    boolean createBitllet(Bitllet bit, EntityManager entity);
+    boolean updateBitllet(Bitllet bit, EntityManager entity);
+    boolean deleteBitllet(Bitllet bit, EntityManager entity);
 
 
 
-    ArrayList<Localitat> TotsLoc(Connection con);
-    Localitat cercaLocalitat(int id, Connection con);
+    ArrayList<Compra> TotsCom(EntityManager entity);
+    Compra cercaCompra(int id, EntityManager entity);
 
-    boolean createLocalitat(Localitat loc, Connection con);
-    boolean updateLocalitat(Localitat loc, Connection con);
-    boolean deleteLocalitat(Localitat loc, Connection con);
-
-
-    ArrayList<Transport> TotsTran(Connection con);
-    Transport cercaTransport(int id, Connection con);
-
-    boolean createTransport(Transport tra, Connection con);
-    boolean updateTransport(Transport tra, Connection con);
-    boolean deleteTransport(Transport tra, Connection con);
+    boolean createCompra(Compra com, EntityManager entity);
+    boolean updateCompra(Compra com, EntityManager entity);
+    boolean deleteCompra(Compra com, EntityManager entity);
 
 
-    ArrayList<Viatge> TotsVia(Connection con);
-    Viatge cercaViatge(int id, Connection con);
+    ArrayList<Equipatge> TotsEquip(EntityManager entity);
+    Equipatge cercaEquipatge(int id, EntityManager entity);
 
-    boolean createViatge(Viatge via, Connection con);
-    boolean updateViatge(Viatge via, Connection con);
-    boolean deleteViatge(Viatge via, Connection con);
+    boolean createEquipatge(Equipatge equ, EntityManager entity);
+    boolean updateEquipatge(Equipatge equ, EntityManager entity);
+    boolean deleteEquipatge(Equipatge equ, EntityManager entity);
+
+    ArrayList<FacEquip> TotsFequip(EntityManager entity);
+    FacEquip cercaFacEquipatge(int id, EntityManager entity);
+
+    boolean createFacEquipatge(FacEquip feq, EntityManager entity);
+    boolean updateFacEquipatge(FacEquip feq, EntityManager entity);
+    boolean deleteFacEquipatge(FacEquip feq, EntityManager entity );
+
+
+
+    ArrayList<Localitat> TotsLoc(EntityManager entity );
+    Localitat cercaLocalitat(int id, EntityManager entity );
+
+    boolean createLocalitat(Localitat loc, EntityManager entity );
+    boolean updateLocalitat(Localitat loc, EntityManager entity );
+    boolean deleteLocalitat(Localitat loc, EntityManager entity);
+
+
+    ArrayList<Transport> TotsTran(EntityManager entity );
+    Transport cercaTransport(int id,  EntityManager entity);
+
+    boolean createTransport(Transport tra, EntityManager entity );
+    boolean updateTransport(Transport tra, EntityManager entity );
+    boolean deleteTransport(Transport tra, EntityManager entity );
+
+
+    ArrayList<Viatge> TotsVia( EntityManager entity);
+    Viatge cercaViatge(int id, EntityManager entity );
+
+    boolean createViatge(Viatge via, EntityManager entity );
+    boolean updateViatge(Viatge via, EntityManager entity );
+    boolean deleteViatge(Viatge via,  EntityManager entity);
 
 
 
